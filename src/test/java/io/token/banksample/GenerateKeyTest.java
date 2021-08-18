9e88af7bb1db0be03761118bd445f11efbe0add4
Odose <72551744+odose-token@users.noreply.github.com>
2021-08-18 20:53:34 +0200

    Added snippet to generate key id for a given public key (#71)
    * Added snippet to generate key id for a given public key
    
    * Removed confusing print
    
    Co-authored-by: DanielS-Token <80842942+DanielS-Token@users.noreply.github.com>

--- src/test/java/io/token/banksample/GenerateKeyTest.java
+++ src/test/java/io/token/banksample/GenerateKeyTest.java
@@ -1,11 +1,14 @@
 package io.token.banksample;
 
+
+import com.google.common.hash.Hashing;
 import io.token.security.crypto.Crypto;
 import io.token.security.crypto.CryptoRegistry;
 import io.token.security.crypto.CryptoType;
 
 import java.security.KeyPair;
 
+import io.token.util.codec.ByteEncoding;
 import org.junit.Test;
 
 public class GenerateKeyTest {
@@ -20,5 +23,20 @@ public class GenerateKeyTest {
         System.out.println("public-key: "
                 + crypto.serialize(keyPair.getPublic())
                 + "  // Give to Token so that Token can verify bank auth payloads");
+
+        System.out.println("Key-ID for public key: " +keyIdFor(crypto.serialize(keyPair.getPublic())));
     }
+
+
+    static String keyIdFor(String serializedKey) {
+        byte[] encoded = ByteEncoding.parse(serializedKey);
+        byte[] hash = Hashing.sha256().newHasher()
+                .putBytes(encoded)
+                .hash()
+                .asBytes();
+        return ByteEncoding.serialize(hash).substring(0, 16);
+    }
+
+
 }
+
